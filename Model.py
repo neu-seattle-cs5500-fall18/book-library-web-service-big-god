5c8e58170c8ba28ac3974c47f1c90c32e24b1823
Bicheng Xu <xbc010@gmail.com>
2018-10-09 18:42:18 -0700

    Updated database model

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.inspection import inspect
+import enum
 
 db = SQLAlchemy()
 
@@ -14,7 +15,21 @@ class User(db.Model):
 
 class Author(db.Model):
     AutherID = db.Column(db.Integer, primary_key=True)
-    AutherName = db.Column(db.String(80), unique=False, nullable=False)
+    FirstName = db.Column(db.String(80), unique=False, nullable=False)
+    LastName = db.Column(db.String(80), unique=False, nullable=False)
+
+    def serialize(self):
+        res = Serializer.serialize(self)
+        return res
+
+class Genre(enum.Enum):
+    Science = 'Science'
+    Horror = 'Horror'
+
+class List(db.Model):
+    ListId = db.Column(db.Integer, primary_key=True)
+    ListName = db.Column(db.String(80), unique=False, nullable=False)
+    Created = db.Column(db.DateTime, unique=False, nullable=True)
 
     def serialize(self):
         res = Serializer.serialize(self)
@@ -22,16 +37,59 @@ class Author(db.Model):
 
 class Book(db.Model):
     BookID = db.Column(db.Integer, primary_key=True)
+    OwnerID = db.Column(db.Integer, db.ForeignKey(User.userId),nullable=False)
     BookName = db.Column(db.String(80), unique=False, nullable=False)
-    AutherID = db.Column(db.Integer, db.ForeignKey('Author.AutherID'),nullable=False)
-    OwnerID = db.Column(db.Integer, db.ForeignKey('User.userId'),nullable=False)
     PublishDate = db.Column(db.DateTime, unique=False, nullable=False)
-    GenreType = db.Column(db.String(80), unique=False, nullable=False)
     LoanedOut = db.Column(db.Boolean, unique=False, nullable=False)
-    LoanedUserID = db.Column(db.Integer, unique=False, nullable=True)
-    ExpectedReturnDate = db.Column(db.DateTime, unique=False, nullable=True)
+
+    def serialize(self):
+        res = Serializer.serialize(self)
+        return res
+
+class Note(db.Model):
+    NoteId = db.Column(db.Integer, primary_key=True)
+    BookId = db.Column(db.Integer, db.ForeignKey(Book.BookID),nullable=False)
+    Content = db.Column(db.String(300), unique=False, nullable=False)
+
+    def serialize(self):
+        res = Serializer.serialize(self)
+        return res
+
+class LoanHistory(db.Model):
+    LoanId = db.Column(db.Integer, primary_key=True)
+    BookId = db.Column(db.Integer, db.ForeignKey(Book.BookID),nullable=False)
+    BorrowerId = db.Column(db.Integer, db.ForeignKey(User.userId),nullable=False)
+    Due = db.Column(db.DateTime, unique=False, nullable=True)
     ActualReturnDate = db.Column(db.DateTime, unique=False, nullable=True)
-    Note = db.Column(db.String(300), unique=False, nullable=True)
+
+    def serialize(self):
+        res = Serializer.serialize(self)
+        return res
+
+
+
+class BookToAuthors(db.Model):
+    BookToAuthorsMapId = db.Column(db.Integer, primary_key=True)
+    BookId = db.Column(db.Integer, db.ForeignKey(Book.BookID),nullable=False)
+    AutherID = db.Column(db.Integer, db.ForeignKey(Author.AutherID),nullable=False)
+
+    def serialize(self):
+        res = Serializer.serialize(self)
+        return res
+
+class BookToGenres(db.Model):
+    BookToGenresId = db.Column(db.Integer, primary_key=True)
+    BookId = db.Column(db.Integer, db.ForeignKey(Book.BookID),nullable=False)
+    Genre = db.Column(db.Enum(Genre))
+
+    def serialize(self):
+        res = Serializer.serialize(self)
+        return res
+
+class ListToBooks(db.Model):
+    ListToBooks = db.Column(db.Integer, primary_key=True)
+    ListId = db.Column(db.Integer, db.ForeignKey(List.ListId),nullable=False)
+    BookId = db.Column(db.Integer, db.ForeignKey(Book.BookID),nullable=False)
 
     def serialize(self):
         res = Serializer.serialize(self)
